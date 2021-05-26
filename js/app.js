20e1e4b152d26530e0513723b08eca5ce7f25b6c
LaithAbayat <laithabayat@gmail.com>
2021-05-26 03:04:21 +0300

    New Update for lab03

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 'use strict';
-
-var name = prompt('Please enter your Name and Surname','Your Name');
+let score = 0;
+let userName = prompt('Please enter your Name and Surname','Your Name');
 {
-  alert('Welcome to the website ' + name);
+  alert('Welcome to the website ' + userName);
+  score++;
 }
 
 let soccer=prompt('Do you like Soccer?');
-if(soccer ==='yes'){
+if(soccer ==='yes' || soccer === 'y'){
   alert('Me too');
-}else if(soccer ==='no'){
+  score++;
+}else if(soccer ==='no' || soccer ==='n'){
   alert('Really? :( I like it very much!');
 }else{
   alert('Next time type yes or no!');
@@ -17,10 +19,13 @@ if(soccer ==='yes'){
 let books = prompt('Do you like Books?');
 switch(books.toLowerCase()){
 case 'yes':
-  console.log('books')
+case 'y':
+  console.log('books');
   alert('Thats Great');
+  score++;
   break;
 case 'no':
+case 'n':
   alert('You must trying to read some books!');
   break;
 default:
@@ -30,10 +35,13 @@ default:
 let Pal = prompt('Do you support Palestine?');
 switch(Pal.toLowerCase()){
 case 'yes':
-  console.log('pal')
+case 'y':
+  console.log('pal');
   alert('Free Palestine');
+  score++;
   break;
 case 'no':
+case 'n':
   alert('Go to Hell!');
   break;
 default:
@@ -43,9 +51,12 @@ default:
 let travel = prompt('Do you like Travel?');
 switch(travel.toLowerCase()){
 case 'yes':
+case 'y':
   alert('Me too i adore it :)');
+  score++;
   break;
 case 'no':
+case 'n':
   alert('You must trying to travel!');
   break;
 default:
@@ -53,17 +64,29 @@ default:
 }
 
 let barca = prompt('Do you like Barcelona?');
-switch(travel.toLowerCase()){
+switch(barca.toLowerCase()){
 case 'yes':
+case 'y':
   alert('Viva Barca! :)');
+  score++;
   break;
 case 'no':
+case 'n':
   alert(':0');
   break;
 default:
   alert('Next time plz type yes or no');
 }
 
+let favMovie = ['Scarface', 'The Godfather','V For Vendetta','The Hobbit','Lord of the rings'];
+favMovie = prompt('Guess my favorite movie from this list: Scarface, The Godfather, V For Vendetta, The Hobbit, Lord of the rings, The First Time !');
+
+while(favMovie !== 'Scarface'){
+  favMovie = prompt('Wrong guess :( try again .. '+'  Guess my favorite movie from this list: Scarface, The Godfather, V For Vendetta, The Hobbit, Lord of the rings, The First Time !');
+}
+
 {
-  alert('Thank You, ' + name);
+
+  alert(userName +' your score is '+score);
+  alert('Thank You, ' + userName);
 }
